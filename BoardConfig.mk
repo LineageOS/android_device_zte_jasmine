146072a0b0b5339705249a49e8b07652e24c70f1
Tom Marshall <tdm.code@gmail.com>
2018-08-03 09:30:21 -0700

    jasmine: Remove selinux permissive from kernel cmdline
    Change-Id: I46565b1e7f82523082bafd6dcdaca55680ac47ef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,7 +49,6 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk ramoops.mem_address=0x9fd00000 ramoops.mem_size=0x200000 ramoops.console_size=0x100000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
